c6cad1b55942a5f9a837f69dd86d4643c25fcb72
sergey.tyuterev <sergey.tyuterev@datadvance.net>
2015-12-30 15:23:42 +0300

    add day 5 silver level

--- Advent/5.py
+++ Advent/5.py
@@ -1,13 +1,58 @@
 import argparse
 
 
-def main(args):
-    with open(args.file_name, 'r') as input_file:
-        pass
+BAD_SUBSTRINGS = ('ab', 'cd', 'pq', 'xy')
+VOWELS = ('a', 'e', 'i', 'o', 'u')
+
+
+def file_analyze(filename):
+    with open(filename, 'r') as input_file:
+        nice_count = 0
+        for line in input_file:
+            nice_count += full_check_silver(line)
+            print '----'
+        print 'There is %d nice strings.' % nice_count
+
+
+def full_check_silver(string):
+    if (check_enough_vowels(string) or
+       check_bad_substrings(string) or
+       check_double_letters(string)):
+        return 0
+    return 1  # Nice
+
+
+def check_enough_vowels(string, vowel_count=3):
+    count = 0
+    for vowel in VOWELS:
+        count += string.count(vowel)
+    if count < vowel_count:
+        return 1  # Naughty
+    return 0
+
+
+def check_bad_substrings(string):
+    for substring in BAD_SUBSTRINGS:
+        if string.find(substring) != -1:
+            return 1  # Naughty
+    return 0
+
+
+def check_double_letters(string):
+    string_length = len(string)
+    for count in xrange(1, string_length):
+        if string[count - 1] == string[count]:
+            return 0
+    return 1  # Naughty
+
+
+def main():
+    # Change the string to a lower case just in case.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', action='store', dest='file_name')
+    args = parser.parse_args()
+    file_analyze(args.file_name)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', action='store', dest='file_name')
-    arguments = parser.parse_args()
-    main(arguments)
+    main()
