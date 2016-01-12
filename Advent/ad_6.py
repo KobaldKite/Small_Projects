c3a1cc0d12a36d9f62677a7e1c770dae9b04ef68
sergey.tyuterev <sergey.tyuterev@datadvance.net>
2016-01-12 15:54:07 +0300

    add more functions to day 6 silver

--- Advent/ad_6.py
+++ Advent/ad_6.py
@@ -14,38 +14,53 @@ class LightGrid(object):
 
     def edit_square(self, coordinates, operation):
         # Coordinates have ((x, y), (x, y)) format
-        point_1, point_2 = coordinates
+        point_1, point_2 = coordinates  # TODO: Letters instead of "words" here! D:
         for y in xrange(point_1[1], point_2[1]):
             for element in xrange(y * self.sizes[0] + point_1[0],
                                   y * self.sizes[0] + point_2[0]):
                 self.grid[element] = operation(self.grid[element])
 
-    def follow_instructions(self, instructions):
-        pass
+    def parse_instruction_file(self, file_name):
+        with open(file_name) as input_file:
+            instructions = input_file.read().splitlines()
+            self.follow_instructions(instructions)
+
+    def follow_instructions(self, instructions):  # I don't like that it copied the whole "instructions"
+        for instruction in instructions:
+            self.follow_instruction(instruction)
 
     def follow_instruction(self, instruction):
-        operation = choose_command(instruction[0])
-        coordinates = (instruction[1], instruction[2])
+        parsed_instruction = parse_instruction(instruction)
+        operation = choose_command(parsed_instruction[0])
+        coordinates = (parsed_instruction[1], parsed_instruction[2])
         self.edit_square(coordinates, operation)
+
+    def count_lit(self):
+        lit_count = 0
+        for element in self.grid:
+            lit_count += element
+        print lit_count
 
 
 def parse_instruction(string):  # TODO: add some more format checks (?)
     #  turn off 660,55 through 986,197
     #  toggle 537,781 through 687,941
-    #  turn on 226,196 through 599,390
-    instruction = string.split()
+    #  turn on 226,196 through 599,390 TODO: use "try - except" here
+    instruction = string.split(" ")
     if instruction[0] == 'toggle':
-        pack_instruction(instruction, ONE_WORD)  # Command is one word long
+        return pack_instruction(instruction, ONE_WORD)  # Command is one word long
     elif instruction[0] == 'turn':
-        pack_instruction(instruction, TWO_WORDS)  # Command is two words long
+        return pack_instruction(instruction, TWO_WORDS)  # Command is two words long
     else:
         print 'Wrong file format.'
 
 
-def pack_instruction(instruction, command_word_count):  # TODO: dispose of these magic numbers!
+def pack_instruction(instruction, command_word_count):
     command = instruction[command_word_count + COMMAND_SHIFT_1]
-    point_1 = [int(number) for number in instruction[command_word_count]]
-    point_2 = [int(number) for number in instruction[command_word_count + COMMAND_SHIFT_2]]
+    point_1 = [int(number) for number
+               in instruction[command_word_count].split(',')]
+    point_2 = [int(number) for number
+               in instruction[command_word_count + COMMAND_SHIFT_2].split(',')]
     return command, point_1, point_2
 
 
@@ -70,15 +85,13 @@ def true_stub(some_element):
     return True
 
 
-def read_file(file_path):
-    with open(file_path) as input_file:
-        pass  # Read all the lines into a tuple
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', action='store', dest='file_name')
     args = parser.parse_args()
+    grid = LightGrid()
+    grid.parse_instruction_file(args.file_name)
+    grid.count_lit()
 
 
 if __name__ == '__main__':
