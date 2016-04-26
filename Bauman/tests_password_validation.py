b3d49f4a2b84d6e180ae701e124f5f4f5490db23
sergey.tyuterev <sergey.tyuterev@datadvance.net>
2016-04-26 11:36:24 +0300

    add password validation tests

---
+++ Bauman/tests_password_validation.py
@@ -0,0 +1,25 @@
+import unittest
+import password_validation
+
+
+class TestGeneral(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_no_upper(self):
+        self.assertFalse(password_validation.validate('asdfg%%%123'))
+
+    def test_no_lower(self):
+        self.assertFalse(password_validation.validate('ASDFG%%%123'))
+
+    def test_no_digit(self):
+        self.assertFalse(password_validation.validate('asdFG%%%JJJ'))
+
+    def test_no_symbol(self):
+        self.assertFalse(password_validation.validate('asdFGJJJ123'))
+
+    def test_too_short(self):
+        self.assertFalse(password_validation.validate('asD%%23'))
+
+    def test_good_password(self):
+        self.assertTrue(password_validation.validate('asdFG%%%123'))
