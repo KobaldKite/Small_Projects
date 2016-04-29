448f0f5d439b128454b6ca167db40f87f65aab48
sergey.tyuterev <sergey.tyuterev@datadvance.net>
2016-04-29 15:49:30 +0300

    add education analyze homework

--- Bauman/edu_analyze.py
+++ Bauman/edu_analyze.py
@@ -1,27 +1,37 @@
 import urllib
 import json
+import io
 import collections
 
 
-def get_okrug_list():
-    response = urllib.urlopen('http://python.aori.ru/data/technical_education_moscow.json')
+def data_from_source(method, address):
+    if method == 'url':
+        return urllib.urlopen(address)
+    elif method == 'path':
+        return io.open(address)
+    else:
+        raise ValueError('Wrong source fetch method!')
+
+
+def get_district_list(method='url', address='http://python.aori.ru/data/technical_education_moscow.json'):
+    response = data_from_source(method, address)
     edu_data = json.load(response)
     return [element.get('Cells').get('okrug') for element in edu_data]
 
 
-def select_most_common(okrug_list):
+def select_most_common(district_list, most_common_count=5):
     """
-    If several okrugs are tied for the 5th position,
+    If several districts are tied for the 5th position,
     only one of them is shown.
     """
-    okrug_count = collections.Counter(okrug_list)
-    return [element[0] for element in okrug_count.most_common(5)]
+    district_count = collections.Counter(district_list)
+    return [element[0] for element in district_count.most_common(most_common_count)]
 
 
 def main():
-    okrug_list = get_okrug_list()
-    okrug_most_common = select_most_common(okrug_list)
-    for element in okrug_most_common:
+    district_list = get_district_list()
+    district_most_common = select_most_common(district_list)
+    for element in district_most_common:
         print element
 
 
