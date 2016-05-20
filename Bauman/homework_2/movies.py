73a26cc847d0922ad74ccb612185cc8ff7dda4c1
sergey.tyuterev <sergey.tyuterev@datadvance.net>
2016-05-20 18:02:21 +0300

    movie adviser further development

--- Bauman/homework_2/movies.py
+++ Bauman/homework_2/movies.py
@@ -5,6 +5,7 @@ import argparse
 import random
 import requests
 import bs4
+import fuzzywuzzy
 
 HEADER = ('movie id', 'movie title', 'release date', 'video release date',
           'IMDb URL', 'unknown', 'Action', 'Adventure', 'Animation', 'Children\'s',
@@ -25,18 +26,43 @@ def get_random_movie(movie_list):
         print 'There is no movies that satisfy your criteria.'
         return ''
     else:
-        return suggested.get('movie title'), get_movie_rating(suggested)
+        return suggested
+
+
+def check_exact_name(movie, soup):
+    find_header = soup.find(class_='findHeader')
+    if find_header:
+        print 'Problems'
+        return try_to_find(movie, soup)
+    print 'Alright'
+    return soup
+
+
+def try_to_find(movie, soup):
+    similar_movies = soup.find_all(class_='result_text')
+    for similar_movie in similar_movies:
+        soup_movie = bs4.BeautifulSoup(str(similar_movie), 'html.parser')
+        if soup_movie.get_text() == movie['movie title']:  # use fuzzywuzzy to get the best ration here
+            movie_raw_html = requests.get(soup_movie.a['href']).text
+            new_soup = bs4.BeautifulSoup(movie_raw_html, 'html.parser')
+            return new_soup
+
+
+def parse_movie_page(movie):
+    movie_url = movie.get('IMDb URL')
+    movie_raw_html = requests.get(movie_url).text
+    soup = bs4.BeautifulSoup(movie_raw_html, 'html.parser')
+    return check_exact_name(movie, soup)
 
 
 def get_movie_rating(movie):
-    movie_url = movie.get('IMDb URL')
-    movie_page_html = requests.get(movie_url).text
-    return movie_page_html
+    soup = parse_movie_page(movie)
+    rating = soup.find(itemprop='ratingValue').contents[0]
+    return rating
 
 
-def get_image(page='http://www.imdb.com/title/tt0115956/'):
-    raw_html = requests.get(page).text
-    soup = bs4.BeautifulSoup(raw_html, 'html.parser')
+def get_movie_image(movie):
+    soup = parse_movie_page(movie)
     image = soup.find(class_='poster').a.img['src']
     return image
 
@@ -62,7 +88,7 @@ class MovieAdviser(object):
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--genre', action='store', dest='genre', default='War')
+    parser.add_argument('--genre', action='store', dest='genre', default='Drama')
     parser.add_argument('--year', action='store', dest='year', default='1996')
     return parser.parse_args()
 
@@ -72,9 +98,9 @@ def main():
     adviser = MovieAdviser()
     selected_movies = adviser.get_movies_by_year_and_genre(arguments.genre, arguments.year)
     print len(selected_movies)
-    print get_random_movie(selected_movies)
-    print selected_movies
-    print get_image()
+    movie = get_random_movie(selected_movies)
+    print get_movie_image(movie)
+    print get_movie_rating(movie)
 
 
 if __name__ == '__main__':
