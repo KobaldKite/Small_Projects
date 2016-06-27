cf7bbf336cb0dfb25bfc6ed835c8f29e924bc7c1
sergey.tyuterev <sergey.tyuterev@datadvance.net>
2016-06-27 11:57:16 +0300

    tests 1 and 3 improve file work

--- Advent/tests_1.py
+++ Advent/tests_1.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import ad_1
 
@@ -14,17 +15,33 @@ DIRECTIONS_SAMPLES = (
     ('(((()((', 5, -1)           # Location = 5, cellar = -1 (never)
 )
 
+TEMP_FILE_NAMES = (
+    ['_'.join(['file', str(number)]) for number, _ in enumerate(DIRECTIONS_SAMPLES)]
+)
+
+
+def clear():
+    for temp_file_name in TEMP_FILE_NAMES:
+        if os.path.isfile(temp_file_name):
+            os.remove(temp_file_name)
+
 
 class TestGeneral(unittest.TestCase):
-    def check_samples(self, task_type):
-        for sample in DIRECTIONS_SAMPLES:
-            self.santa = ad_1.Santa(task_type)
-            self.assertEqual(self.santa.follow_directions(sample[0]),
-                             sample[TASKS[task_type]])
+    def setUp(self):
+        clear()
+        for sample_number, direction_sample in enumerate(DIRECTIONS_SAMPLES):
+            with open(TEMP_FILE_NAMES[sample_number], 'w') as temp_file:
+                temp_file.write(direction_sample[0])
+
+    def tearDown(self):
+        clear()
 
     def test_general(self):
-        self.check_samples('SILVER')
-        self.check_samples('GOLD')
+        for temp_file, direction_sample in zip(TEMP_FILE_NAMES, DIRECTIONS_SAMPLES):
+            for task_type in ad_1.TYPE:
+                santa = ad_1.Santa(task_type)
+                self.assertEqual(santa.do_the_job(temp_file),
+                                 direction_sample[TASKS.get(task_type)])
 
 
 if __name__ == '__main__':
--- Advent/tests_3.py
+++ Advent/tests_3.py
@@ -3,6 +3,11 @@ import unittest
 import ad_3
 
 
+TASKS = {
+    'SILVER': 1,
+    'GOLD': 2
+}
+
 DIRECTIONS_SAMPLES = (
     ('<<<<<<', 7, 4),      # 7 houses for one Santa, 4 houses for two
     ('<><><>', 2, 7),
@@ -26,22 +31,18 @@ class TestGeneral(unittest.TestCase):
     def setUp(self):
         clear()
         for sample_number, directions_sample in enumerate(DIRECTIONS_SAMPLES):
-            temp_file_name = TEMP_FILE_NAMES[sample_number]
-            with open(temp_file_name, 'w') as temp_file:
+            with open(TEMP_FILE_NAMES[sample_number], 'w') as temp_file:
                 temp_file.write(directions_sample[0])
 
     def tearDown(self):
         clear()
 
-    def check(self, task_type):
-        for sample_number, directions_sample in enumerate(DIRECTIONS_SAMPLES):
-            del_plan = ad_3.DeliveryPlan(task_type)
-            self.assertEqual(del_plan.do_the_job(TEMP_FILE_NAMES[sample_number]),
-                             directions_sample[ad_3.TYPE[task_type]])
-
     def test_general(self):
-        self.check('SILVER')
-        self.check('GOLD')
+        for temp_file, direction_sample in zip(TEMP_FILE_NAMES, DIRECTIONS_SAMPLES):
+            for task_type in ad_3.TYPE:
+                delivery_plan = ad_3.DeliveryPlan(task_type)
+                self.assertEqual(delivery_plan.do_the_job(temp_file),
+                                 direction_sample[TASKS.get(task_type)])
 
 
 if __name__ == '__main__':
